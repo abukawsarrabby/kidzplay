c601d23bc71abf0dd72f4042336c8b4c5f7d4a69
Abu-kawsar-rabby <rabbyafrad24@.com>
2023-05-21 20:38:54 +0600

    search implement on all toy page

--- src/components/AddToy.jsx
+++ src/components/AddToy.jsx
@@ -7,13 +7,11 @@ import PageTitle from './PageTitle';
 const AddToy = () => {
 
     const categories = useLoaderData();
-
     const { user, createUser } = useContext(AuthContext);
     const navigate = useNavigate();
 
     const handleAddToy = event => {
         event.preventDefault();
-
         const form = event.target;
         const userName = user?.displayName;
         const toyName = form.toyName.value;
@@ -62,84 +60,88 @@ const AddToy = () => {
                 console.log(data);
             })
     }
+
+
     return (
-        <div className="hero min-h-screen bg-gray-100">
+        <div>
             <PageTitle title='Add New Toy'></PageTitle>
-            <div className="container mx-auto py-8">
-                <div className="max-w-4xl mx-auto">
-                    <div className="card shadow-lg bg-white p-6">
-                        <h1 className="text-3xl font-bold mb-3">Add New Toy!</h1>
-                        <form onSubmit={handleAddToy}>
-                            <div className="grid md:grid-cols-2 gap-4">
-                                <div className="form-control">
-                                    <label className="label">
-                                        <span className="label-text">Toy Name</span>
-                                    </label>
-                                    <input type="text" name="toyName" placeholder="Toy Name" className="input input-bordered" required />
+            <div className="hero min-h-screen bg-gray-100">
+                <div className="container mx-auto py-8">
+                    <div className="max-w-4xl mx-auto">
+                        <div className="card shadow-lg bg-white p-6">
+                            <h1 className="text-3xl font-bold mb-3">Add New Toy!</h1>
+                            <form onSubmit={handleAddToy}>
+                                <div className="grid md:grid-cols-2 gap-4">
+                                    <div className="form-control">
+                                        <label className="label">
+                                            <span className="label-text">Toy Name</span>
+                                        </label>
+                                        <input type="text" name="toyName" placeholder="Toy Name" className="input input-bordered" required />
+                                    </div>
+                                    <div className="form-control">
+                                        <label className="label">
+                                            <span className="label-text">Seller Name</span>
+                                        </label>
+                                        <input type="text" name="sellerName" placeholder="Seller Name" defaultValue={user?.displayName} readOnly className="input input-bordered" />
+                                    </div>
+                                    <div className="form-control">
+                                        <label className="label">
+                                            <span className="label-text">Seller Email</span>
+                                        </label>
+                                        <input type="text" name="sellerEmail" placeholder="Seller Email" readOnly defaultValue={user?.email} className="input input-bordered" />
+                                    </div>
+                                    <div className="form-control">
+                                        <label className="label">
+                                            <span className="label-text">Sub-category</span>
+                                        </label>
+                                        <select className="select select-bordered w-full" name="subCategory" required>
+                                            <option disabled value="" defaultValue="" className="font-bold">Choose A Category</option>
+                                            {
+                                                categories.map(category => (
+                                                    <option key={category._id} value={category.category} className="font-bold">
+                                                        {category.category}
+                                                    </option>
+                                                ))
+                                            }
+                                        </select>
+                                    </div>
+                                    <div className="form-control">
+                                        <label className="label">
+                                            <span className="label-text">Price</span>
+                                        </label>
+                                        <input type="text" name="price" placeholder="$10" className="input input-bordered" required />
+                                    </div>
+                                    <div className="form-control">
+                                        <label className="label">
+                                            <span className="label-text">Rating</span>
+                                        </label>
+                                        <input type="text" name="rating" placeholder="Rating" className="input input-bordered" />
+                                    </div>
+                                    <div className="form-control">
+                                        <label className="label">
+                                            <span className="label-text">Quantity</span>
+                                        </label>
+                                        <input type="number" name="quantity" placeholder="100" className="input" required />
+                                    </div>
+                                    <div className="form-control">
+                                        <label className="label">
+                                            <span className="label-text">Picture URL</span>
+                                        </label>
+                                        <input type="text" name="pictureUrl" placeholder="Picture URL" className="input input-bordered" required />
+                                    </div>
                                 </div>
                                 <div className="form-control">
                                     <label className="label">
-                                        <span className="label-text">Seller Name</span>
+                                        <span className="label-text">Product Description</span>
                                     </label>
-                                    <input type="text" name="sellerName" placeholder="Seller Name" defaultValue={user?.displayName} readOnly className="input input-bordered" />
-                                </div>
-                                <div className="form-control">
-                                    <label className="label">
-                                        <span className="label-text">Seller Email</span>
-                                    </label>
-                                    <input type="text" name="sellerEmail" placeholder="Seller Email" readOnly defaultValue={user?.email} className="input input-bordered" />
-                                </div>
-                                <div className="form-control">
-                                    <label className="label">
-                                        <span className="label-text">Sub-category</span>
-                                    </label>
-                                    <select className="select select-bordered w-full" name="subCategory" required>
-                                        <option disabled value="" defaultValue="" className="font-bold">Choose A Category</option>
-                                        {
-                                            categories.map(category => (
-                                                <option key={category._id} value={category.category} className="font-bold">
-                                                    {category.category}
-                                                </option>
-                                            ))
-                                        }
-                                    </select>
-                                </div>
-                                <div className="form-control">
-                                    <label className="label">
-                                        <span className="label-text">Price</span>
-                                    </label>
-                                    <input type="text" name="price" placeholder="$10" className="input input-bordered" required />
-                                </div>
-                                <div className="form-control">
-                                    <label className="label">
-                                        <span className="label-text">Rating</span>
-                                    </label>
-                                    <input type="text" name="rating" placeholder="Rating" className="input input-bordered" />
-                                </div>
-                                <div className="form-control">
-                                    <label className="label">
-                                        <span className="label-text">Quantity</span>
-                                    </label>
-                                    <input type="number" name="quantity" placeholder="100" className="input" required />
-                                </div>
-                                <div className="form-control">
-                                    <label className="label">
-                                        <span className="label-text">Picture URL</span>
-                                    </label>
-                                    <input type="text" name="pictureUrl" placeholder="Picture URL" className="input input-bordered" required />
-                                </div>
-                            </div>
-                            <div className="form-control">
-                                <label className="label">
-                                    <span className="label-text">Product Description</span>
-                                </label>
-                                <textarea name="productDescription" placeholder="Description is here.......
+                                    <textarea name="productDescription" placeholder="Description is here.......
                                 " className="textarea textarea-bordered h-24" required></textarea>
-                            </div>
-                            <div className="form-control mt-10">
-                                <button className="btn-kidzplay text-white">Submit</button>
-                            </div>
-                        </form>
+                                </div>
+                                <div className="form-control mt-10">
+                                    <button className="btn-kidzplay text-white">Submit</button>
+                                </div>
+                            </form>
+                        </div>
                     </div>
                 </div>
             </div>
--- src/components/AllToys.jsx
+++ src/components/AllToys.jsx
@@ -1,14 +1,37 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useLoaderData } from 'react-router-dom';
 import Modal from './Modal';
 import PageTitle from './PageTitle';
 
 const AllToys = () => {
     const toys = useLoaderData();
+    const [searchQuery, setSearchQuery] = useState(toys);
+
+    const handleSearch = event => {
+        event.preventDefault();
+        const query = event.target.value;
+
+        fetch(`http://localhost:5000/search?toyName=${query}`)
+            .then(res => res.json())
+            .then(data => {
+                setSearchQuery(data);
+            });
+    };
+
     return (
         <div>
             <PageTitle title='All Toys'></PageTitle>
             <h1 className='text-5xl text-center font-bold my-10'>Total toys: {toys?.length}</h1>
+            <div className='text-right my-5'>
+                <input
+                    type="text"
+                    placeholder="Search by Toy Name"
+                    className="input input-bordered input-error w-full mr-5 max-w-xs"
+                    // value={searchQuery}
+                    onChange={handleSearch}
+                />
+                <button className='btn-kidzplay' onClick={handleSearch}>Search</button>
+            </div>
             <div className="overflow-x-auto">
                 <table className="table table-compact w-full">
                     <thead>
@@ -23,22 +46,17 @@ const AllToys = () => {
                         </tr>
                     </thead>
                     <tbody>
-                        {toys?.map((toy, index) => (
+                        {searchQuery?.map((toy, index) => (
                             <tr key={index}>
-                                <th>{index + 1}</th>
+                                <td>{index + 1}</td>
                                 <td>{toy?.sellerName}</td>
                                 <td>{toy?.toyName}</td>
                                 <td>{toy?.subCategory}</td>
                                 <td>{'$' + toy?.price}</td>
                                 <td>{toy?.quantity}</td>
                                 <td>
-                                    <label
-                                        htmlFor={`my-modal-${toy?._id}`} className="btn-kidzplay">View Details
-                                    </label>
-                                    <Modal
-                                        key={toy?._id}
-                                        toy={toy}
-                                    ></Modal>
+                                    <label htmlFor={`my-modal-${toy?._id}`} className="btn-kidzplay">View Details</label>
+                                    <Modal key={toy?._id} toy={toy}></Modal>
                                 </td>
                             </tr>
                         ))}
